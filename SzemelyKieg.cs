0abeb7418aa7431031090935ad4afa2fa1df39d8
csokanandor95 <csokanandor77@gmail.com>
2025-04-24 21:24:11 +0200

    Create SzemelyKieg.cs

---
+++ SzemelyKieg.cs
@@ -0,0 +1,10 @@
+﻿namespace cnTelefonkonyv
+{
+    public partial class Szemely
+    {
+        public string? HelységNév => Helység?.Név;
+        public short? HelységIrányítószám => Helység?.Irányítószám;
+        public string TelefonszámLista => Számok.Aggregate("",
+            (c, a) => c + (c.Length > 0 ? ", " : "") + a.SzámSztring);
+    }
+}
